1667593cd4f55f3f5a548427f0b7049512656bd3
Andrew Chen <andrew.chen@mongodb.com>
2022-10-20 15:01:35 -0400

    add stuff

---
+++ bulkWrite_test.go
@@ -0,0 +1,80 @@
+package writes_test
+
+import (
+	"context"
+	"fmt"
+	"testing"
+	"time"
+
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+func createUpdate(numElements int) []mongo.WriteModel {
+	writes := make([]mongo.WriteModel, 0, numElements)
+
+	for i := 0; i < numElements; i++ {
+		writes = append(writes, mongo.NewUpdateOneModel().
+			SetFilter(bson.D{{"name", fmt.Sprintf("name%d", time.Now().Unix())}}).
+			SetUpdate(bson.D{{"%set", bson.D{{"foo", "bar"}}}}),
+		)
+	}
+	return writes
+}
+
+func BenchmarkBulkWrite(b *testing.B) {
+
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+	client, err := mongo.Connect(ctx, options.Client().ApplyURI("mongodb://localhost:27017"))
+	if err != nil {
+		b.Errorf("failed to create client: %s", err)
+	}
+
+	coll := client.Database("test").Collection("test")
+
+	b.Run("when writing 1000 updates", func(b *testing.B) {
+
+		b.Run("bulkWrite in batches of 1", func(b *testing.B) {
+			for i := 0; i < 1000; i++ {
+				b.StopTimer()
+				updates := createUpdate(1)
+				b.StartTimer()
+				coll.BulkWrite(context.Background(), updates)
+				b.StopTimer()
+			}
+		})
+
+		b.Run("bulkWrite in batches of 10", func(b *testing.B) {
+			for i := 0; i < 100; i++ {
+				b.StopTimer()
+				updates := createUpdate(10)
+				b.StartTimer()
+				coll.BulkWrite(context.Background(), updates)
+				b.StopTimer()
+			}
+		})
+
+		b.Run("bulkWrites in batches of 100", func(b *testing.B) {
+			for i := 0; i < 10; i++ {
+				b.StopTimer()
+				updates := createUpdate(100)
+				b.StartTimer()
+				coll.BulkWrite(context.Background(), updates)
+				b.StopTimer()
+			}
+		})
+
+		b.Run("bulkWrites in batches of 1000", func(b *testing.B) {
+			for i := 0; i < 1; i++ {
+				b.StopTimer()
+				updates := createUpdate(1000)
+				b.StartTimer()
+				coll.BulkWrite(context.Background(), updates)
+				b.StopTimer()
+			}
+		})
+
+	})
+}
